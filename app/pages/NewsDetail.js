5562dabe97f2edb57ab1f51bc131a0050b1a0442
lihao <591353643@qq.com>
2017-05-18 00:42:50 +0800

    加入Image选择器，Android端需要更改选择器Module的build.gradle，保证项目与Module的版本一致 --Lihao 20170518

--- app/pages/NewsDetail.js
+++ app/pages/NewsDetail.js
@@ -6,17 +6,62 @@
  */
 
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
     StyleSheet,
     Text,
     View
 } from 'react-native';
-import {Button} from 'antd-mobile';
+import { Button } from 'antd-mobile';
 import Toolbar from '../container/Toolbar';
+import ImagePicker from 'react-native-image-picker';
 
+const options = {
+    title: '请选择图片来源',
+    cancelButtonTitle: '取消',
+    takePhotoButtonTitle: '拍照',
+    chooseFromLibraryButtonTitle: '相册图片',
+    customButtons: [
+        { name: 'hangge', title: 'hangge.com图片' },
+    ],
+    storageOptions: {
+        skipBackup: true,
+        path: 'images'
+    }
+};
 
 class NewsDetail extends Component {
+
+    constructor(props) {
+        super(props);
+        this.openImagePicker = this.openImagePicker.bind(this);
+    }
+
+    openImagePicker() {
+        ImagePicker.showImagePicker(options, (response) => {
+            console.log('Response = ', response);
+
+            if (response.didCancel) {
+                console.log('用户取消了选择！');
+            }
+            else if (response.error) {
+                alert("ImagePicker发生错误：" + response.error);
+            }
+            else if (response.customButton) {
+                alert("自定义按钮点击：" + response.customButton);
+            }
+            else {
+                let source = { uri: response.uri };
+                // You can also display the image using data:
+                // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+                this.setState({
+                    avatarSource: source
+                });
+            }
+        });
+
+    }
+
     render() {
         return (
             <View style={styles.container}>
@@ -30,6 +75,7 @@ class NewsDetail extends Component {
                         height: 40,
                         width: 100
                     }}
+                    onClick={this.openImagePicker}
                 >
                     确定
                 </Button>
